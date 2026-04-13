27cc3931ab6e186aa4d9becee17917d6c60756b8
Rupraj Baral <ruprajbaral@gmail.com>
2026-04-13 19:13:06 +0545

    init

---
+++ pef.py
@@ -0,0 +1,65 @@
+# making a custom image extension :3
+
+def encoder(width,height,pixels,path): #RGBA as a hash is supported with 4 channels
+    with open(path,'wb') as file:
+        if width*height!=len(pixels) or width<=0 or height<=0 or any(len(pixel)!=8 for pixel in pixels):
+            raise ValueError('Invalid input')
+        file.write(b'PEF\x00') # defining the extension type
+        file.write(width.to_bytes(4,'big')) # 'big' so that the order remains the same
+        file.write(height.to_bytes(4,'big'))
+        for pixel in pixels:
+            r,g,b,a=int(pixel[:2],16),int(pixel[2:4],16),int(pixel[4:6],16),int(pixel[6:8],16)
+            file.write(r.to_bytes(1,'big'))
+            file.write(g.to_bytes(1,'big'))
+            file.write(b.to_bytes(1,'big'))
+            file.write(a.to_bytes(1,'big'))
+
+def decoder(path):
+    with open(path,'rb') as file:
+        if file.read(4)!=b'PEF\x00':
+            raise ValueError('Not a valid PEF file')
+        w_b=file.read(4)
+        if len(w_b)!=4:
+            raise ValueError('Invalid width data')
+        h_b=file.read(4)
+        if len(h_b)!=4:
+            raise ValueError('Invalid height data')
+        width=int.from_bytes(w_b,'big')
+        height=int.from_bytes(h_b,'big')
+
+        pixels=[] # empty list
+
+        total=width*height # total referring to total number of pixels
+
+        for _ in range(total):
+            r_b=file.read(1)
+            if len(r_b)!=1:
+                raise ValueError('Invalid pixel data')
+            g_b=file.read(1)
+            if len(g_b)!=1:
+                raise ValueError('Invalid pixel data')
+            b_b=file.read(1)
+            if len(b_b)!=1:
+                raise ValueError('Invalid pixel data')
+            a_b=file.read(1)
+            if len(a_b)!=1:
+                raise ValueError('Invalid pixel data')
+
+            r_int=int.from_bytes(r_b,'big')
+            g_int=int.from_bytes(g_b,'big')
+            b_int=int.from_bytes(b_b,'big')
+            a_int=int.from_bytes(a_b,'big')
+
+
+            # 0 as padding, 2 as size, X as uppercase hex
+            r=format(r_int,'02X') 
+            g=format(g_int,'02X')
+            b=format(b_int,'02X')
+            a=format(a_int,'02X')
+
+            hexcode=r+g+b+a
+            pixels.append(hexcode)
+
+
+        return width,height,pixels
+
